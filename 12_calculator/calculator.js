dc80d2b4fa62e8896bad8cf2f9507fc913373f25
kitkatz111 <164080724+kitkatz111@users.noreply.github.com>
2025-10-31 10:29:14 +0000

    calculator completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,14 +1,28 @@
-const add = function() {
-	
+const add = function(number, numbertwo) {
+	resultAdd = number + numbertwo;
+  return resultAdd;
 };
 
-const subtract = function() {
-	
+const subtract = function(number, numbertwo) {
+	resultSubtract = number - numbertwo;
+  return resultSubtract;
 };
 
-const sum = function() {
-	
-};
+const sum = function sumIntegers(...numbers) {
+   for(let arg of numbers) {
+
+    let result = 0;
+      for(let i = 0; i < arg.length; ++i){
+        result += arg[i];
+      }
+    console.log(result)
+    return result; 
+  } 
+
+  
+  
+  };
+  
 
 const multiply = function() {
 
